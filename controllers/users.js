4663b8ea5b5ddd4da7809dd9bfd92635a3b04888
jhersbo <jhersbo@gmail.com>
2022-08-25 14:54:54 -0500

    work on users controller

--- controllers/users.js
+++ controllers/users.js
@@ -3,4 +3,22 @@ const db = require('../models')
 const { Op } = require('sequelize')
 const { users, items } = db
 
+
+//login verification
+
+router.get('/', async (req, res)=>{
+    try{
+        const foundUser = await users.findAll({
+            where:{
+                username: req.body.username,
+                password: req.body.password
+            }
+        })
+        res.status(200).json(foundUser)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
 module.exports = router
