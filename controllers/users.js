ea3f5a548dc73ff02a6c052485c10700222b24a4
jhersbo <jhersbo@gmail.com>
2022-08-25 17:33:46 -0500

    finished users controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,6 @@ const { users, items } = db
 
 
 //login verification
-
 router.get('/', async (req, res)=>{
     try{
         const foundUser = await users.findAll({
@@ -16,6 +15,46 @@ router.get('/', async (req, res)=>{
         })
         res.status(200).json(foundUser)
     }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//add a user account
+router.post('/', async (req, res)=>{
+    try{
+        users.create(req.body)
+        res.status(200).json('User created')
+    }catch(err){
+        res.status(500).json(err)
+        console.log(err)
+    }
+})
+
+//edit user information
+router.put('/', async (req, res)=>{
+    try{
+        await users.update(req.body, {
+            where:{
+                user_id: req.body.user_id
+            }
+        })
+        res.status(200).json(`Successfully edited user's data.`)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//delete user
+router.delete('/', async (req, res)=>{
+    try{
+        users.destroy({
+            where: {
+                user_id: req.body.user_id
+            }
+        })
+        res.status(200).json("User deleted.")
+    }
+    catch(err){
         res.status(500).json(err)
     }
 })
