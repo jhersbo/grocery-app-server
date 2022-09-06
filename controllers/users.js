28ae97f4aed06c5fcdd2a4e01249b45d50fc5343
jhersbo <jhersbo@gmail.com>
2022-09-06 16:17:20 -0500

    blah

--- controllers/users.js
+++ controllers/users.js
@@ -22,17 +22,6 @@ router.get('/', async (req, res)=>{
     }
 })
 
-//add a user account
-// router.post('/', async (req, res)=>{
-//     try{
-//         users.create(req.body)
-//         res.status(200).json('User created')
-//     }catch(err){
-//         res.status(500).json(err)
-//         console.log(err)
-//     }
-// })
-
 //add user account and hash password
 router.post('/', (req, res)=>{
     bcrypt.hash(req.body.password, saltRounds, async (err, hash)=>{
