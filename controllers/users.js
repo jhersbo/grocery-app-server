2c9bd8315bfb80c72abbcc5f658998c305bb77a3
jhersbo <jhersbo@gmail.com>
2022-09-06 16:17:03 -0500

    added server-side password encryption and auth

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,14 @@ const db = require('../models')
 const { Op } = require('sequelize')
 const { users, items } = db
 
+//bcrypt
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+const encryption = (password)=>{
+    bcrypt.hash(password, saltRounds)
+}
+
 
 //retrieve users 
 router.get('/', async (req, res)=>{
@@ -15,14 +23,53 @@ router.get('/', async (req, res)=>{
 })
 
 //add a user account
-router.post('/', async (req, res)=>{
-    try{
-        users.create(req.body)
-        res.status(200).json('User created')
-    }catch(err){
-        res.status(500).json(err)
-        console.log(err)
-    }
+// router.post('/', async (req, res)=>{
+//     try{
+//         users.create(req.body)
+//         res.status(200).json('User created')
+//     }catch(err){
+//         res.status(500).json(err)
+//         console.log(err)
+//     }
+// })
+
+//add user account and hash password
+router.post('/', (req, res)=>{
+    bcrypt.hash(req.body.password, saltRounds, async (err, hash)=>{
+        try{
+            await users.create({
+            user_id: req.body.user_id,
+            name: req.body.name,
+            username: req.body.username,
+            password: hash
+            })
+            res.status(200).json('User created')
+        }catch(err){
+            res.status(500).json(err)
+            console.log(err)
+        }
+    })
+})
+
+//password authentication
+router.post('/auth', async (req, res)=>{
+    await users.findOne({
+        where: {
+            user_id: req.body.user_id
+        }
+    }).then((user)=>{
+        if(!user){
+            res.status(204).json('No user found.')
+        }else{
+            bcrypt.compare(req.body.password, user.password, (err, result)=>{
+                if(result){
+                    res.status(200).json('User authenticated.')
+                }else{
+                    res.status(401).json('Incorrect password.')
+                }
+            })
+        }
+    })
 })
 
 //edit user information
